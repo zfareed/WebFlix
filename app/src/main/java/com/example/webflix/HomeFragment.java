0f3a74bd271daa4b0e52d73f495b26c4ed43c9b3
Zain Fareed <zainfareed093@gmail.com>
2020-12-08 17:05:33 -0800

    Created Slideshow

--- app/src/main/java/com/example/webflix/HomeFragment.java
+++ app/src/main/java/com/example/webflix/HomeFragment.java
@@ -18,23 +18,34 @@ public class HomeFragment extends Fragment {
     ViewFlipper flipper;
 
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        view =  inflater.inflate(R.layout.fragment_home, container, false);
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        flipper = view.findViewById(R.id.flipper);
+
+        int slideshowArray[] = {R.drawable.slider, R.drawable.webflix_logo, R.drawable.slider};
+
+        for (int i = 0; i < slideshowArray.length; i++)
+            slideShow(slideshowArray[i]);
 
 
-
-
-
-
-
-       return view;
+        return view;
     }
 
-   
+    public void slideShow(int img) {
+        ImageView imageView = new ImageView(getContext());
+        imageView.setBackgroundResource(img);
+
+        flipper.addView(imageView);
+        flipper.setFlipInterval(3000);
+        flipper.setAutoStart(true);
+
+        flipper.setInAnimation(getContext(), android.R.anim.slide_in_left);
+        flipper.setInAnimation(getContext(), android.R.anim.slide_out_right);
 
 
+    }
 }
