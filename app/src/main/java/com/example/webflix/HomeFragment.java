6a720d0a7965dfd1398219a3bb340dd43bd343f2
Zain Fareed <zainfareed093@gmail.com>
2020-12-08 17:04:11 -0800

    Created Simple Drawer Layout make some customization.

---
+++ app/src/main/java/com/example/webflix/HomeFragment.java
@@ -0,0 +1,40 @@
+package com.example.webflix;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
+
+
+public class HomeFragment extends Fragment {
+
+    View view;
+    ViewFlipper flipper;
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view =  inflater.inflate(R.layout.fragment_home, container, false);
+
+
+
+
+
+
+
+       return view;
+    }
+
+   
+
+
+}
